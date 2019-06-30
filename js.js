e2555eca573bdaa5e6ef2dcb9fa4e6c42a9a0740
Kacper Kuczewski <kucza06@wp.pl>
2019-06-30 15:46:49 +0200

    add add and romve task fn and refresh array fn

--- js.js
+++ js.js
@@ -1,10 +1,39 @@
-const ul = document.querySelector('ul');
-const li = document.querySelector('li');
-const input = document.querySelector('input');
-const button = document.querySelector('button');
+const ul = document.querySelector("ul");
+const lis = document.querySelectorAll("li");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const form = document.querySelector("form");
+const deleteButton = document.querySelector(".delete");
 
-const taskArray = [
-    'gitara siema',
-    'superkacper4',
-    'siema isema'
-],
+const taskArray = [];
+
+const addTask = e => {
+  e.preventDefault();
+  const inputValue = input.value;
+  const task = document.createElement("li");
+  ul.appendChild(task);
+  task.innerHTML = inputValue + `<button class="delete">Delete</button>`;
+  taskArray.push(task);
+  refreshArray();
+  input.value = "";
+  task.querySelector("button").addEventListener("click", removeTask);
+};
+
+const removeTask = e => {
+  const index = e.target.parentNode.dataset.key;
+  taskArray.splice(index, 1);
+  console.log("123");
+  refreshArray();
+};
+
+const refreshArray = () => {
+  ul.textContent = "";
+  taskArray.forEach((element, key) => {
+    element.dataset.key = key;
+    ul.appendChild(element);
+  });
+};
+
+form.addEventListener("submit", addTask);
+
+// deleteButton.addEventListener("click", removeTask);

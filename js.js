ae53060edb15061812fbf85cfac0096bde9875de
Kacper Kuczewski <kucza06@wp.pl>
2019-06-30 14:14:42 +0200

    add style and html file

---
+++ js.js
@@ -0,0 +1,10 @@
+const ul = document.querySelector('ul');
+const li = document.querySelector('li');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+const taskArray = [
+    'gitara siema',
+    'superkacper4',
+    'siema isema'
+],
